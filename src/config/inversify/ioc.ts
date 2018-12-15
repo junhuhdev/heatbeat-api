2ec766ca3433cc535250f957d6e821fec70c99f6
Jun Huh <juan.huh@hotmail.com>
2018-12-15 03:36:17 +0100

    ff

--- src/config/inversify/ioc.ts
+++ src/config/inversify/ioc.ts
@@ -4,10 +4,12 @@ import { autoProvide, buildProviderModule } from "inversify-binding-decorators";
 import TYPES from "./Types";
 import { IMedicationService, MedicationService } from "../../services";
 import { IUserService, UserService } from "../../services/UserService";
+import { MongoDBClient } from "../../database/mongodb/Client";
 
 const container = new Container();
 
 container.bind<IMedicationService>(TYPES.MedicationService).to(MedicationService);
 container.bind<IUserService>(TYPES.UserService).to(UserService);
+container.bind<MongoDBClient>(TYPES.MongoDBClient).to(MongoDBClient);
 
 export { container, autoProvide, inject };
