846dae5b6c42d340e6966dea2d276aecf41db89d
Jun Huh <juan.huh@hotmail.com>
2018-12-15 02:56:34 +0100

    completed medicationservice

--- src/services/MedicationService.ts
+++ src/services/MedicationService.ts
@@ -7,8 +7,10 @@ import { MongoDBClient } from "../database/mongodb/Client";
 
 export interface IMedicationService {
     findById(id: string): Promise<Medication>;
-
     findAllByManufacturer(manufacturer: string): Promise<Medication[]>;
+    create(medication: Medication): Promise<Medication>;
+    update(id: string, medication: Medication): Promise<Medication>;
+    delete(id: string): Promise<any>;
 }
 
 @provide(TYPES.MedicationService)
@@ -30,6 +32,32 @@ export class MedicationService implements IMedicationService {
     public findById(id: string): Promise<Medication> {
         return new Promise<Medication>((resolve, reject) => {
             this.mongoClient.findOneById(MedicationsCollection, id, (error: any, data: any) => {
+                resolve(data);
+            });
+        });
+    }
+
+    public create(medication: Medication): Promise<Medication> {
+        return new Promise<Medication>((resolve, reject) => {
+            this.mongoClient.insert(MedicationsCollection, medication, (error, data: Medication) => {
+                resolve(data);
+            });
+        });
+    }
+
+    public update(id: string, medication: Medication): Promise<Medication> {
+        return new Promise<Medication>((resolve, reject) => {
+            this.mongoClient.update(MedicationsCollection, id, medication, (error, data: Medication) => {
+                this.mongoClient.update(MedicationsCollection, id, medication, (error, data: Medication) => {
+                    resolve(data);
+                });
+            });
+        });
+    }
+
+    public delete(id: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.mongoClient.remove(MedicationsCollection, id, (error, data: any) => {
                 resolve(data);
             });
         });
