330dcf229823f484089d3a1533cd1c0c81aa0192
Jun Huh <juan.huh@hotmail.com>
2019-01-16 21:50:27 +0100

    small fix

--- src/app.ts
+++ src/app.ts
@@ -46,7 +46,6 @@ const resolvers = {
 const apolloServer = new ApolloServer({typeDefs, resolvers});
 apolloServer.applyMiddleware({app});
 
-
 // Connect to MongoDB
 const mongoUrl = MONGODB_URI;
 (<any>mongoose).Promise = bluebird;
