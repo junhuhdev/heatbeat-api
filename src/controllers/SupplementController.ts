203e33a9dbd994d077e589ce3ba4138699018104
Jun Huh <juan.huh@evry.com>
2019-02-16 23:51:05 +0100

    hotfix 2

--- src/controllers/SupplementController.ts
+++ src/controllers/SupplementController.ts
@@ -31,7 +31,7 @@ export class SupplementController {
 
     @Put("/:id")
     putTwice(@Param("id") id: number, @Body() sup2plement: any) {
-        return "";
+        return "2";
     }
 
     @Put("/:id")
